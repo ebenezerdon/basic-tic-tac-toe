8f851836acceb93a92d83ff9fb3f6e1f92fbcfad
Ebenezer Don <ebenezerdonu@gmail.com>
2019-10-10 04:04:51 +0100

    improve app functionality:
    - fix issue with game end notice
    - improve AI for computer moves
    - add reload button and reset game functionality

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const allCells = Array.from(document.getElementsByClassName('cell-value'));
+const winStatus = document.getElementById('winStatus');
 let userFilledCells = [];
 let computerFilledCells = [];
+let turn = 'user';
+let gameOver = false;
 let winCombinationArray = [
   [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8],
   [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6]
@@ -8,37 +11,83 @@ let winCombinationArray = [
 
 const playGame = cell => {
   const indexOfCell = [].indexOf.call(allCells, cell);
-  if (cell.innerHTML.length < 1) {
+  if (!gameOver && cell.innerHTML.length < 1) {
     (function playUserTurn() {
       cell.innerHTML = '<p>x</p>';
       userFilledCells.push(indexOfCell);
       userFilledCells = [...new Set(userFilledCells)].sort();
       winCombinationArray.forEach(winCombination => {
         if (winCombination.every(val => userFilledCells.includes(val))){
-          alert('you win!');
-          window.location.reload();
-          winCombinationArray = [];
+          gameOver = true;
+          winStatus.style.display = 'block';
+          winStatus.innerHTML = 'You win!';
         }
       });
     })();
 
-    (function playComputerTurn() {
+    !gameOver && (function playComputerTurn() {
       const allCellIndexes = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-        const filledCellIndexes = [...computerFilledCells, ...userFilledCells];
-        const emptyCellIndexes = allCellIndexes.filter(cell => !filledCellIndexes.includes(cell));
-        const randomEmptyCellIndex = emptyCellIndexes[
-          Math.floor(Math.random() * emptyCellIndexes.length) + 0
-        ];
-        allCells[randomEmptyCellIndex].innerHTML = '<p>o</p>';
-        computerFilledCells.push(randomEmptyCellIndex);
-        winCombinationArray.forEach(winCombination => {
-          if (winCombination.every(val => computerFilledCells.includes(val))){
-            alert('you lose!');
-            window.location.reload();
-          }
-        });
+      const filledCellIndexes = [...computerFilledCells, ...userFilledCells];
+      const emptyCellIndexes = allCellIndexes.filter(cell => !filledCellIndexes.includes(cell));
+      const randomEmptyCellIndex = emptyCellIndexes[
+        Math.floor(Math.random() * emptyCellIndexes.length) + 0
+      ];
+      let computerWinCell, userWinCell;
+
+      if (emptyCellIndexes.length < 1) {
+        gameOver = true;
+        winStatus.style.display = 'block';
+        winStatus.innerHTML = "It's a tie!";
+      }
+
+        computerFilledCells.length > 1 && winCombinationArray.some(winCombination => {
+          const intersection = winCombination.filter(element => computerFilledCells.includes(element));
+          intersection.length > 1 && winCombination.map(element => {
+            if(!intersection.includes(element) && emptyCellIndexes.includes(element)) {
+              computerWinCell = element;
+            }
+          });
+
+          return (intersection.length > 1 && computerWinCell);
+        })
+
+        !computerWinCell && userFilledCells.length > 1 && winCombinationArray.some(winCombination => {
+          const intersection = winCombination.filter(element => userFilledCells.includes(element));
+          intersection.length > 1 && winCombination.map(element => {
+            if(!intersection.includes(element) && emptyCellIndexes.includes(element)) {
+              userWinCell = element;
+            }
+          });
+
+          return (intersection.length > 1 && userWinCell);
+        })
+
+      // zero is falsy, hence the comparison to undefined
+      const cellToBePlayed = (computerWinCell != undefined ? computerWinCell
+        : userWinCell != undefined ? userWinCell
+        : randomEmptyCellIndex);
+
+      allCells[cellToBePlayed].innerHTML = '<p>o</p>';
+      computerFilledCells.push(cellToBePlayed);
+      winCombinationArray.forEach(winCombination => {
+        if (winCombination.every(val => computerFilledCells.includes(val))){
+          gameOver = true;
+          winStatus.style.display = 'block';
+          winStatus.innerHTML = 'You lose!';
+        }
+      });
     })();
   }
 };
 
+const replayButton = document.getElementById('replayButton');
+const replayGame = () => {
+  allCells.map(cell => cell.innerHTML = '');
+  userFilledCells = [];
+  computerFilledCells = [];
+  winStatus.style.display = 'none';
+  gameOver = false;
+}
+
 allCells.map(cell => cell.addEventListener('click', () => playGame(cell)));
+replayButton.addEventListener('click', () => replayGame());
